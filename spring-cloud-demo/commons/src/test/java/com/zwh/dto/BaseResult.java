9f5899b8c5f778d8beb895562bae482050ba9aa9
wenhao.zhu <wenhao.zhu@derbysoft.com>
2017-11-22 16:02:08 +0800

    commons

---
+++ spring-cloud-demo/commons/src/test/java/com/zwh/dto/BaseResult.java
@@ -0,0 +1,107 @@
+package com.zwh.dto;
+
+import java.io.Serializable;
+
+public class BaseResult<T extends Object> implements Serializable {
+
+    public Boolean success;
+
+    private T content;
+
+    private String message;
+
+    private String code;
+
+
+    public BaseResult() {
+    }
+
+    public BaseResult(T content) {
+        this.content = content;
+        success = true;
+    }
+
+    public BaseResult(String code, String message) {
+        this.message = message;
+        this.code = code;
+        success = false;
+    }
+
+    public BaseResult(Boolean success, String message) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public BaseResult(Boolean success, String code, String message) {
+        this.message = message;
+        this.code = code;
+        this.success = success;
+    }
+
+    public BaseResult(Boolean success, T content, String code, String message) {
+        this.message = message;
+        this.code = code;
+        this.content = content;
+        this.success = success;
+    }
+
+    public BaseResult<T> success(String message){
+        this.setMessage(message);
+        this.setSuccess(true);
+        return this;
+    }
+
+    public BaseResult<T> failure(String message){
+        this.setMessage(message);
+        this.setSuccess(false);
+        return this;
+    }
+
+    public BaseResult<T> content(T content){
+        this.setContent(content);
+        return this;
+    }
+
+    public BaseResult<T> code(String code){
+        this.setCode(code);
+        return this;
+    }
+
+    public BaseResult<T> successContent(T t) {
+        this.content(t);
+        this.success = true;
+        return this;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
